928bb52968eb19f291fe5c69547b06b54bba036e
cotg <50111657+ccs55@users.noreply.github.com>
2019-06-01 19:07:57 +0700

    Change List User

--- resources/views/admin/user/list.blade.php
+++ resources/views/admin/user/list.blade.php
@@ -1,35 +1,66 @@
 @extends('layouts.admin')
-
 @section('title','List User')
-<div class="container">
-    @section('content')
+<div>
+  @section('content')
 
+  <!-- Begin Page Content -->
+  <div class="container-fluid">
+
+    <!-- Page Heading -->
     <a href={{url('/admin/user/create')}}>
-        <button class="btn btn-outline-primary">
-            Create User
-        </button>
+      <button class="btn btn-outline-primary" style="margin-bottom : 5px">
+        Create User
+      </button>
     </a>
 
-    @foreach ($user as $item)
-    <div class="row">
-        <div class="col-md-8">
-            <h4>{{$item->name}}</h4>
+    <!-- DataTales Example -->
+    <div class="card shadow mb-4">
+      <div class="card-header py-3">
+        <h6 class="m-0 font-weight-bold text-primary">Users</h6>
+      </div>
+      <div class="card-body">
+        <div class="table-responsive">
+          <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th width=10%>Edit</th>
+                <th width=10%>Delete</th>
 
+              </tr>
+            </thead>
+
+            <tbody>
+              @foreach ($user as $item)
+              <tr>
+                <td>
+                  {{$item->name}}
+                </td>
+                <td>
+                  <form method="GET" action={{url('/admin/user/'.$item->id.'/edit')}}>
+                    @csrf
+                    <button type="submit">Edit</button>
+                  </form>
+                </td>
+                <td>
+                  <form method="POST" action={{url('/admin/user/'.$item->id)}}>
+                    @method('DELETE')
+                    @csrf
+                    <button type="submit">Delete</button>
+                  </form>
+                </td>
+
+              </tr>
+              @endforeach
+
+            </tbody>
+          </table>
         </div>
-        <div class="col-md-2">
-            <form method="GET" action={{url('/admin/user/'.$item->id.'/edit')}}>
-                @csrf
-                <button type="submit">Edit</button>
-            </form>
-        </div>
-        <div class="col-md-2">
-            <form method="POST" action={{url('/admin/user/'.$item->id)}}>
-                @method('DELETE')
-                @csrf
-                <button type="submit">Delete</button>
-            </form>
-        </div>
+      </div>
     </div>
-    @endforeach
-    @endsection
+
+  </div>
+  <!-- /.container-fluid -->
+  <!-- End of Main Content -->
+  @endsection
 </div>
